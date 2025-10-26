517dcd6aa9cb15850975984d601c706b44987273
ifine2002 <boyyhp1@gmail.com>
2025-10-26 20:53:45 +0700

    update dashboard

--- src/pages/admin/dashboard.jsx
+++ src/pages/admin/dashboard.jsx
@@ -1,46 +1,57 @@
 import { Card, Col, Row, Statistic } from "antd";
-import CountUp from 'react-countup';
+import { useEffect, useState } from "react";
+import CountUp from "react-countup";
+import { callGetDashboard } from "../../api/services";
 
 const DashboardPage = () => {
-    const formatter = (value) => {
-        return (
-            <CountUp end={Number(value)} separator="," />
-        );
-    };
+  const [data, setData] = useState({});
 
-    return (
-        <Row gutter={[20, 20]}>
-            <Col span={24} md={8}>
-                <Card title="Card title" bordered={false} >
-                    <Statistic
-                        title="Active Users"
-                        value={112893}
-                        formatter={formatter}
-                    />
+  const formatter = (value) => {
+    return <CountUp end={Number(value)} separator="," />;
+  };
 
-                </Card>
-            </Col>
-            <Col span={24} md={8}>
-                <Card title="Card title" bordered={false} >
-                    <Statistic
-                        title="Active Users"
-                        value={112893}
-                        formatter={formatter}
-                    />
-                </Card>
-            </Col>
-            <Col span={24} md={8}>
-                <Card title="Card title" bordered={false} >
-                    <Statistic
-                        title="Active Users"
-                        value={112893}
-                        formatter={formatter}
-                    />
-                </Card>
-            </Col>
+  useEffect(() => {
+    fetchDashboard();
+  }, []);
 
-        </Row>
-    )
-}
+  const fetchDashboard = async () => {
+    const res = await callGetDashboard();
+    if (res && res.status === 200) {
+      setData(res.data);
+    }
+  };
+
+  return (
+    <Row gutter={[20, 20]}>
+      <Col span={24} md={8}>
+        <Card title="Total Users" bordered={false}>
+          <Statistic
+            // title="Total Users"
+            value={data.totalUser}
+            formatter={formatter}
+          />
+        </Card>
+      </Col>
+      <Col span={24} md={8}>
+        <Card title="Total Books" bordered={false}>
+          <Statistic
+            // title="Total Books"
+            value={data.totalBook}
+            formatter={formatter}
+          />
+        </Card>
+      </Col>
+      <Col span={24} md={8}>
+        <Card title="Total Reviews" bordered={false}>
+          <Statistic
+            // title="Total Reviews"
+            value={data.totalReview}
+            formatter={formatter}
+          />
+        </Card>
+      </Col>
+    </Row>
+  );
+};
 
 export default DashboardPage;
